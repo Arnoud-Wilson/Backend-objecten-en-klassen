b2da7fc41519073d0fe6c8a1dc6212167180d6d8
Arnoud-Wilson <arnoud608@gmail.com>
2023-05-24 19:49:50 +0200

    finished bonus assignment

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
 
         ApplePieRecipe applePieRecipe = new ApplePieRecipe();
+        StrawberryPie strawberryPie = new StrawberryPie();
 
         applePieRecipe.printApplePieIngredients();
         applePieRecipe.printApplePieInstructions();
+
+        strawberryPie.printIngredientsStrawberryPie();
     }
 }
 
--- src/StrawberryPie.java
+++ src/StrawberryPie.java
@@ -1,4 +1,16 @@
 package src;
 
 public class StrawberryPie {
+    Ingredients strawberry = new Ingredients(250, "gram", "aardbeien");
+    Ingredients butter = new Ingredients(125, "gram", "boter");
+    Ingredients sugar = new Ingredients(100, "gram", "suiker");
+
+    public void printIngredientsStrawberryPie() {
+        System.out.println("De ingredienten voor de aarbeien taart zijn:");
+        System.out.println(strawberry.getAmount() + ", " + strawberry.getUnit() + ", " + strawberry.getName());
+        System.out.println(butter.getAmount() + ", " + butter.getUnit() + ", " + butter.getName());
+        System.out.println(sugar.getAmount() + ", " + sugar.getUnit() + ", " + sugar.getName());
+    }
 }
+
+
